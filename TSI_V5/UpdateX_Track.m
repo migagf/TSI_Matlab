0e311439c6c2d250f05ca1604c5a17d1da7dc216
miguelgomez-ucb <miguel.gomez.f@berkeley.edu>
2023-11-30 11:52:02 -0800

    mgf

--- TSI_V5/UpdateX_Track.m
+++ TSI_V5/UpdateX_Track.m
@@ -1,8 +1,17 @@
 % This subroutine calculates XTrack and VTrack
 
-BridgeResponse.X_Track(1,ib) = BridgeResponse.X(1,ib-1) - 0.5*BridgeResponse.X(3,ib-1);
-BridgeResponse.X_Track(2,ib) = -0.6*BridgeResponse.X(3,ib-1);
+if on_bridge
+    BridgeResponse.X_Track(1,ib) = BridgeResponse.X(1,ib-1) - 0.5*BridgeResponse.X(3,ib-1);
+    BridgeResponse.X_Track(2,ib) = -0.6*BridgeResponse.X(3,ib-1);
+    
+    BridgeResponse.V_Track(1,ib) = BridgeResponse.Xdot(1,ib-1) - 0.5*BridgeResponse.Xdot(3,ib-1);
+    BridgeResponse.V_Track(2,ib) = -0.6*BridgeResponse.Xdot(3,ib-1);
 
-BridgeResponse.V_Track(1,ib) = BridgeResponse.Xdot(1,ib-1) - 0.5*BridgeResponse.Xdot(3,ib-1);
-BridgeResponse.V_Track(2,ib) = -0.6*BridgeResponse.Xdot(3,ib-1);
+else
+    BridgeResponse.X_Track(1,ib) = 0;
+    BridgeResponse.X_Track(2,ib) = 0;
+    
+    BridgeResponse.V_Track(1,ib) = 0;
+    BridgeResponse.V_Track(2,ib) = 0;
 
+end
